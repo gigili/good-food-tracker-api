34ac8ea4e9bd13291a20c8e28ed2d374ea85beaf
Igor Ilic <igorilicbl@gmail.com>
2020-08-29 20:44:09 +0200

    Start working on register endpoint
    Signed-off-by: Igor Ilic <igorilicbl@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -11,34 +11,46 @@ router.get('/', function (req, res, _) {
 
 router.post("/login", async (req, res, _) => {
 	if (req.body.hasOwnProperty("username") === false || !req.body["username"]) {
-		return res.send(helper.invalid_response("Invalid username"));
+		res.send(helper.invalid_response("Invalid username"));
+		return res.sendStatus(400);
 	}
 
 	if (req.body.hasOwnProperty("password") === false || !req.body["password"]) {
-		return res.send(helper.invalid_response("Invalid password"));
+		res.send(helper.invalid_response("Invalid password"));
+		return res.sendStatus(400);
 	}
 
 	const loginResult = await db.login(req.body["username"], req.body["password"]);
 
 	if (loginResult.success === false) {
-		return res.send(helper.invalid_response("Login operation failed."))
+		res.send(helper.invalid_response("Login operation failed."))
+		return res.sendStatus(400);
 	}
 
 	if (loginResult.hasOwnProperty("rows") === false || loginResult.rows.hasOwnProperty("id") === false) {
-		return res.send(helper.invalid_response("Account doesn't exist."))
+		res.send(helper.invalid_response("Account doesn't exist."))
+		return res.sendStatus(400);
 	}
 
 	const user = await db.getUserData(loginResult.rows.id);
 	if (user.hasOwnProperty("rows") === false || user.rows.hasOwnProperty("id") === false) {
-		return res.send(helper.invalid_response("Account doesn't exist."))
+		res.send(helper.invalid_response("Account doesn't exist."))
+		return res.sendStatus(400);
 	}
 
 	if (parseInt(user.rows.active) === 0) {
-		return res.send(helper.invalid_response("Account is not active. Contact application administrator."))
+		res.send(helper.invalid_response("Account is not active. Contact application administrator."))
+		return res.sendStatus(400);
 	}
 
 	const tokenData = helper.generate_token(user.rows);
 	res.send(tokenData);
+	res.sendStatus(200);
+});
+
+router.post("/register", async (req, res, _) => {
+	res.send(helper.invalid_response("NOT IMPLEMENTED!"));
+	res.sendStatus(500);
 });
 
 module.exports = router;
