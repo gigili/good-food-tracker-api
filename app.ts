74f7565d844b95e6ae3a145cb93a7407ebad2143
kabartolo <adairkate@gmail.com>
2020-11-20 23:08:19 +0800

    Set up docs route and Swagger UI page

--- app.ts
+++ app.ts
@@ -8,6 +8,8 @@ const path = require('path');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fileUpload = require("express-fileupload");
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -16,6 +18,38 @@ const profileRoutes = require('./routes/profile');
 const cityRoutes = require('./routes/city');
 const countryRoutes = require('./routes/country');
 const reviewRoutes = require('./routes/review');
+
+const swaggerDefinition = {
+	openapi: '3.0.0',
+	info: {
+		title: 'Good Food Tracker API',
+		version: '0.0.1',
+		description: `The Good Food Tracker project allows users to take
+			pictures and/or leave notes, ratings, and comments about the
+			restaurants they visit for later reference.`,
+		license: {
+			name: 'Licensed Under MIT',
+			url: 'https://spdx.org/licenses/MIT.html',
+		},
+		contact: {
+			name: 'Igor Ilić',
+			email: 'github@igorilic.net',
+		},
+	},
+	servers: [
+		{
+			url: 'http://localhost:3000',
+			description: 'Development server',
+		},
+	],
+};
+
+const options = {
+	swaggerDefinition,
+	apis: ['./routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 
@@ -63,5 +97,6 @@ app.use('/profile', profileRoutes);
 app.use('/city', cityRoutes);
 app.use('/country', countryRoutes);
 app.use('/review', reviewRoutes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
