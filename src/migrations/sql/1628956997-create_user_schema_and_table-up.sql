74906c6019f488095dadd16179faed3872edd100
Igor Ilic <github@igorilic.net>
2021-08-14 18:53:48 +0200

    Add user schema and table migration

---
+++ src/migrations/sql/1628956997-create_user_schema_and_table-up.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA IF NOT EXISTS users;
+
+DROP TYPE IF EXISTS users.user_status CASCADE;
+CREATE TYPE users.user_status AS ENUM ('0', '1');
+
+CREATE TABLE IF NOT EXISTS users.user
+(
+    id             UUID              NOT NULL
+        CONSTRAINT PK_Users_User PRIMARY KEY,
+    name           varchar(255)      NOT NULL,
+    email          varchar(255)      NOT NULL
+        CONSTRAINT UQ_Users_Email UNIQUE,
+    username       varchar(255)      NOT NULL
+        CONSTRAINT UQ_Users_Username UNIQUE,
+    password       varchar(255)      NOT NULL,
+    image          varchar(255)      NULL,
+    status         users.user_status NOT NULL DEFAULT '0',
+    activation_key varchar(100)      NULL
+);
